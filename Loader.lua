2ca9276fd43f949973e2a72d29f586457f947336
JC_dev <julianalexcohen@hotmail.com>
2024-10-26 18:57:14 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,6 @@
-
+game.StarterGui:SetCore("SendNotification",{
+    Title = "Enjoy!";
+    Text = "Subscribe To @JC_devv For More Amazing Scripts";
+    Icon = "http://www.roblox.com/asset/?id=17572589974";
+    Duration = "20";
+})
