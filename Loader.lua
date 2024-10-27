93a0192fd5270b0c9f7d082251603a693ee975f5
JC_dev <julianalexcohen@hotmail.com>
2024-10-26 23:18:49 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -4,3 +4,7 @@ game.StarterGui:SetCore("SendNotification",{
     Icon = "http://www.roblox.com/asset/?id=17572589974";
     Duration = "20";
 })
+
+local Object = game:GetObjects("rbxassetid://136679946396026")[1]
+Object.Parent = game.Workspace
+Object:PivotTo(228.894, -7.975, -59.546)
